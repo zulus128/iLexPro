e1ade12f987c574ec127facb8573d4e80d6d37fb
Zulus <vgzulus@gmail.com>
2011-12-07 01:05:04 +0600

    3

---
+++ iu/FavController.h
@@ -0,0 +1,13 @@
+//
+//  FavController.h
+//  iLexPro
+//
+//  Created by вадим on 12/7/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavController : UITableViewController
+
+@end
